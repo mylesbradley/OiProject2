ce7abf4dd029d67f943e692f1bf95bc8da041644
Blake Bailey <bfbailey@coe.edu>
2017-04-13 14:43:50 -0500

    Finished final sprint.

--- app/src/main/java/com/example/myles/oiproject2/OiWidget.java
+++ app/src/main/java/com/example/myles/oiproject2/OiWidget.java
@@ -3,26 +3,26 @@ package com.example.myles.oiproject2;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.telephony.SmsManager;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Implementation of App Widget functionality.
  * App Widget Configuration implemented in {@link OiWidgetConfigureActivity OiWidgetConfigureActivity}
  */
-public class OiWidget extends AppWidgetProvider {
+public class OiWidget extends AppWidgetProvider
+{
 
     private static SmsManager smsMgr;
     private TextView test;
@@ -31,6 +31,9 @@ public class OiWidget extends AppWidgetProvider {
     private static ArrayList<String> names = new ArrayList<String>();
     private static ArrayList<String> numbers = new ArrayList<String>();
     private static String message;
+    private static int appWidgetId2;
+    private static SharedPreferences prefs;
+    private static Boolean isPremium;
 
     public static String TEST_FUNCTION = "Test Function";
 
@@ -39,7 +42,10 @@ public class OiWidget extends AppWidgetProvider {
 
         String widgetText = OiWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
 
+        prefs = context.getSharedPreferences("list_Premium", 0);
+        isPremium = prefs.getBoolean("isPremium", false);
 
+        appWidgetId2 = appWidgetId;
 
                 // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.oi_widget);
@@ -89,13 +95,6 @@ public class OiWidget extends AppWidgetProvider {
         return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 
-    private static void sendText(String widgetText){
-        smsMgr = SmsManager.getDefault();
-        String number = widgetText.split(",")[0];
-        String message = widgetText.split(",")[2];
-        smsMgr.sendTextMessage(number, null,"OI: " + message, null, null);
-    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
@@ -130,7 +129,8 @@ public class OiWidget extends AppWidgetProvider {
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent)
+    {
         super.onReceive(context, intent);
 
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
@@ -139,12 +139,71 @@ public class OiWidget extends AppWidgetProvider {
 
         ComponentName oiWidget = new ComponentName(context, OiWidget.class);
 
-        if(intent.getAction() == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
-            for (int i = 0; i < numbers.size(); i++) {
-                String number = numbers.get(i);
-                smsMgr = SmsManager.getDefault();
-                smsMgr.sendTextMessage(number, null, message, null, null);
+        if(intent.getAction() == AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+        {
+            int count = OiWidgetConfigureActivity.loadCounter(context);
+            String savedDate = OiWidgetConfigureActivity.loadDate(context);
+
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yy");
+
+            Date todayDate = new Date();
+            Date storedDate = new Date();
+
+            if (!isPremium)
+            {
+                if (savedDate.equals("None")) {
+                    count = 0;
+                    OiWidgetConfigureActivity.saveDate(context, df.format(todayDate));
+                } else {
+                    try {
+                        storedDate = df.parse(savedDate);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                String month1 = storedDate.toString().split(" ")[1];
+                String date1 = storedDate.toString().split(" ")[2];
+
+                String month2 = todayDate.toString().split(" ")[1];
+                String date2 = todayDate.toString().split(" ")[2];
+
+                boolean sameDay = (month1.equals(month2) && date1.equals(date2));
+
+                if (!sameDay) {
+                    count = 0;
+                    OiWidgetConfigureActivity.saveDate(context, df.format(todayDate));
+                    storedDate = todayDate;
+                }
+
+                if ((storedDate.before(todayDate) || storedDate.compareTo(todayDate) == 0) && count <= 3)
+                {
+                    for (int i = 0; i < numbers.size(); i++)
+                    {
+                        String number = numbers.get(i);
+                        smsMgr = SmsManager.getDefault();
+                        smsMgr.sendTextMessage(number, null, "Oi: " + message, null, null);
+                        count++;
+                    }
+
+
+                }
+                else
+                    Toast.makeText(context, "You've reached your limit for the day. Try again tomorrow!", Toast.LENGTH_SHORT).show();
+
+                OiWidgetConfigureActivity.saveCounter(context, count);
+                OiWidgetConfigureActivity.saveDate(context, df.format(todayDate));
             }
+            else
+            {
+                for (int i = 0; i < numbers.size(); i++)
+                {
+                    String number = numbers.get(i);
+                    smsMgr = SmsManager.getDefault();
+                    smsMgr.sendTextMessage(number, null, message, null, null);
+                }
+            }
+
         }
         appWidgetManager.updateAppWidget(oiWidget, remoteViews);
     }
--- app/src/main/java/com/example/myles/oiproject2/OiWidgetConfigureActivity.java
+++ app/src/main/java/com/example/myles/oiproject2/OiWidgetConfigureActivity.java
@@ -1,21 +1,32 @@
 package com.example.myles.oiproject2;
 
+import android.Manifest;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v7.widget.VectorEnabledTintResources;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.telephony.SmsManager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * The configuration screen for the {@link OiWidget OiWidget} AppWidget.
@@ -28,18 +39,24 @@ public class OiWidgetConfigureActivity extends Activity implements View.OnClickL
 
     private EditText mAppWidgetText;
     private EditText medtNumber;
-    private EditText medtName;
     private EditText medtMessage;
     private Button mbtnContact;
+    private Button mbtnPremium;
+
+    private static SharedPreferences.Editor editor;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor2;
 
     private SmsManager smsMgr;
 
     private String contactID = "";
     private String content = "";
-
+    private String code = "";
+    private Boolean isPremium = false;
 
     private static final int PICK_CONTACT = 1;
     private static final int READ_CONTACTS = 1;
+    private static final int MY_PERMISSIONS_REQUEST_ACCOUNTS = 1;
 
 
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
@@ -104,39 +121,83 @@ public class OiWidgetConfigureActivity extends Activity implements View.OnClickL
     }
 
     @Override
-    public void onCreate(Bundle icicle) {
+    public void onCreate(Bundle icicle)
+    {
         super.onCreate(icicle);
-
-
 
         // Set the result to CANCELED.  This will cause the widget host to cancel
         // out of the widget placement if the user presses the back button.
         setResult(RESULT_CANCELED);
-
-
         setContentView(R.layout.oi_widget_configure);
-
         idControls();
 
         findViewById(R.id.btnContacts).setOnClickListener(this);
-
         findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
 
         // Find the widget id from the intent.
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-        if (extras != null) {
-            mAppWidgetId = extras.getInt(
-                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        if (extras != null)
+        {
+            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
         }
 
         // If this activity was started with an intent without an app widget ID, finish with an error.
-        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID)
+        {
             finish();
             return;
         }
 
+        if (Build.VERSION.SDK_INT < 23) {}
+            //Do not need to check the permission
+        else
+        {
+            if (checkAndRequestPermissions()) {}
+                //If you have already permitted the permission
+        }
+
         mAppWidgetText.setText(loadTitlePref(OiWidgetConfigureActivity.this, mAppWidgetId));
+    }
+
+    private boolean checkAndRequestPermissions()
+    {
+        int permissionContact = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS);
+
+        int permissionSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (permissionContact != PackageManager.PERMISSION_GRANTED)
+            listPermissionsNeeded.add(Manifest.permission.READ_CONTACTS);
+
+        if (permissionSMS != PackageManager.PERMISSION_GRANTED)
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS);
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MY_PERMISSIONS_REQUEST_ACCOUNTS);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case MY_PERMISSIONS_REQUEST_ACCOUNTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){}
+                    //Permission Granted Successfully. Write working code here.
+                else
+                    //You did not accept the request can not use the functionality.
+
+                break;
+        }
     }
 
     private void idControls(){
@@ -144,16 +205,80 @@ public class OiWidgetConfigureActivity extends Activity implements View.OnClickL
         medtMessage = (EditText) findViewById(R.id.edtMessage);
         medtNumber = (EditText) findViewById(R.id.edtNumber);
         mbtnContact = (Button) findViewById(R.id.btnContacts);
-        mbtnContact.setOnClickListener(this); //.setOnClickListener(this);
+        mbtnPremium = (Button) findViewById(R.id.getPremium);
+
+        mbtnPremium.setOnClickListener(this);
+        mbtnContact.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.btnContacts)
+        {
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, PICK_CONTACT);
+        }
+        else if(v.getId() == R.id.getPremium)
+        {
+            sendPremText();
+            addItemPopup();
+        }
+    }
+
+    private void sendPremText()
+    {
+        Random r = new Random();
+        int Low = 0;
+        int High = 9;
+
+        for(int i = 0; i < 4; i++)
+            code += Integer.toString(r.nextInt(High-Low) + Low);
+
         smsMgr = SmsManager.getDefault();
-        //smsMgr.sendTextMessage("8168126045", null,"OI: " + medtMessage.getText().toString(), null, null);
-        //medtMessage.setText("");
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, PICK_CONTACT);
+        smsMgr.sendTextMessage("18168126045", null, "Your code is: " + code, null, null);
+    }
+
+    private void addItemPopup()
+    {
+        LayoutInflater inflater = getLayoutInflater();
+        final View alertLayout = inflater.inflate(R.layout.edit_popup, null);
+
+        prefs = getSharedPreferences("list_Premium", 0);
+        editor2 = prefs.edit();
+
+        final EditText codeInput = (EditText) alertLayout.findViewById(R.id.codeInput);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setView(alertLayout);
+
+        alert.setPositiveButton("Enter", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                if(codeInput.getText().toString().matches(code))
+                {
+                    isPremium = true;
+
+                    editor2.putString("code", code);
+                    editor2.putBoolean("isPremium", isPremium);
+                    editor2.commit();
+
+                    Toast.makeText(getBaseContext(), "Congratulations you're now premium!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+
     }
 
     @Override
@@ -223,36 +348,55 @@ public class OiWidgetConfigureActivity extends Activity implements View.OnClickL
         }
     }
 
-    public static String getContactName(Context context, String phoneNumber) {
+    public static String getContactName(Context context, String phoneNumber)
+    {
         ContentResolver cr = context.getContentResolver();
         Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
         Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
-        if (cursor == null) {
+        if (cursor == null)
             return null;
-        }
-        String contactName = null;
-        if(cursor.moveToFirst()) {
-            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
-        }
 
-        if(cursor != null && !cursor.isClosed()) {
+        String contactName = null;
+
+        if(cursor.moveToFirst())
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+
+        if(cursor != null && !cursor.isClosed())
             cursor.close();
-        }
 
         return contactName;
     }
 
-    static int loadCounter(Context context, int appWidgetId){
-        SharedPreferences pref = context.getSharedPreferences("Counter", 0);
-        int count = pref.getInt("Count", 0);
+    static int loadCounter(Context context)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("Count", 0);
+        int count = prefs.getInt("Count", 0);
+
         return count;
     }
 
-    static void saveCounter(Context context, int appWidgetId, int count){
-        SharedPreferences.Editor pref = context.getSharedPreferences("Count", 0).edit();
-        pref.putInt("Count", count+1);
-        pref.commit();
+    static void saveCounter(Context context, int count)
+    {
+        SharedPreferences prefs = context.getSharedPreferences("Count", 0);
+        editor = prefs.edit();
 
+        editor.putInt("Count", count);
+        editor.apply();
+    }
+
+    static String loadDate(Context context)
+    {
+        SharedPreferences pref = context.getSharedPreferences("Date", 0);
+        String day = pref.getString("Date", "None");
+        return day;
+    }
+
+    static void saveDate(Context context, String date)
+    {
+        SharedPreferences pref = context.getSharedPreferences("Date", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("Date", date);
+        editor.commit();
     }
 }
 
